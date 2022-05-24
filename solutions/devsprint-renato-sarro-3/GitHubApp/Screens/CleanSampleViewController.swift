51fb9b892274a8a29135c83e0ad7e05b0775909d
Renato Sarro <cora@MacBook-Pro-de-Cora-7.local>
2022-05-24 18:27:12 -0300

    Implementa sample de codigo.

---
+++ solutions/devsprint-renato-sarro-3/GitHubApp/Screens/CleanSampleViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CleanSampleViewController.swift
+//  GitHubApp
+//
+//  Created by Renato Sarro on 24/05/22.
+//
+
+import UIKit
+
+class CleanSampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- solutions/devsprint-renato-sarro-3/GitHubApp/Screens/Detail/DetailView.swift
+++ solutions/devsprint-renato-sarro-3/GitHubApp/Screens/Detail/DetailView.swift
@@ -7,6 +7,22 @@
 
 import UIKit
 
-class DetailView: UIView {
+class DetailView: UIView, ViewCode {
+    lazy var button = UIButton()
     
+    func configureViews() {
+        button.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func buildHierarchy() {
+        addSubview(button)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            button.bottomAnchor.constraint(equalTo: bottomAnchor),
+            button.leadingAnchor.constraint(equalTo: leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
 }
